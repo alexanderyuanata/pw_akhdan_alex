ca4a12084bd86cb03ad184085694f47144093a0e
unfocusedlex <92197888+unfocusedlex@users.noreply.github.com>
2022-12-20 13:10:14 +0700

    commit2

--- add.php
+++ add.php
@@ -7,9 +7,44 @@
 
   <!--Begin Contents-->
   <div class="container-fluid" id="add-content">
-    <div class="row">
+    <div class="row fullh">
       <div class="col">
+        <div id="add-div">
+          <form id="add-form">
+            <label>Nama Karyawan:</label>
+            <input type="text" class="form-control add-c" id="in-name" name="nama" maxlength="80" required />
 
+            <label>Jenis Kelamin:</label>
+            <select class="form-select add-c" form="add-form" name="jk">
+              <option value="P">Pria</option>
+              <option value="W">Wanita</option>
+            </select>
+
+            <label>Tanggal Lahir:</label>
+            <input type="date" class="form-control add-c" id="in-bd" name="bd" required />
+
+            <label>Gaji:</label>
+            <input type="number" class="form-control add-c" name="gaji" required />
+
+            <label>Departemen:</label>
+            <select class="form-select add-c" form="add-form" name="dep">
+              <option selected value="Akuntansi">Akuntansi</option>
+              <option value="Logistik">Logistik</option>
+              <option value="Humas">Humas</option>
+              <option value="Desain">Desain</option>
+              <option value="Teknologi">Teknologi</option>
+              <option value="Manajemen">Manajemen</option>
+            </select>
+
+            <label>Jabatan:</label>
+            <select class="form-select add-c" form="add-form" name="jab">
+              <option selected value="Pegawai">Pegawai</option>
+              <option value="Manajer">Manajer</option>
+            </select>
+
+            <button type="submit" class="btn btn-primary add-c">Tambah Pegawai</button>
+          </form>
+        </div>
       </div>
     </div>
   </div>
--- js/index.js
+++ js/index.js
@@ -17,4 +17,23 @@ $(document).ready(function () {
     });
     event.preventDefault();
   });
+
+  $("#add-form").submit(function (event) {
+    var sent_data = {
+      nama: $("#reg-user").val(),
+      reg_pw: $("#reg-pw").val(),
+    };
+
+    $.ajax({
+      type: "POST",
+      url: "register.php",
+      data: sent_data,
+    }).done(function (response) {
+      window.alert(response);
+      if (response == "Akun berhasil dibuat") {
+        window.location.replace("./home.html");
+      }
+    });
+    event.preventDefault();
+  });
 });
